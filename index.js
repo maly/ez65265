65b3f66c2189b505d87cb2a9c28beccdd9319628
Martin Maly <maly@maly.cz>
2017-04-08 11:05:31 +0200

    macOS fix

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var portList = [];
 
 SerialPort.list(function (err, ports) {
   ports.forEach(function(port) {
-  	if (port.vendorId!='0403') return
-  	if (port.productId!='6001') return
+  	if (port.vendorId!='0403' && port.vendorId!='0x0403') return
+  	if (port.productId!='6001' && port.productId!='0x6001') return
     //console.log(port.comName);
     //console.log(port.manufacturer);
     portList.push({id:port.comName})
@@ -56,8 +56,9 @@ app.get('/conn/:port', function (req, res) {
   	console.log("Connected")
   	res.send("Connected"); return;
   }
-  console.log("Try to connect",req.params.port)
-  openPort(req.params.port)
+  var port = decodeURIComponent(req.params.port);
+  console.log("Try to connect",port)
+  openPort(port)
   res.send("Connecting")
 })
 
